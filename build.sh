503d26fe7f0f9c3bee8b783b12716a30555d206c
KAsp3rd <casper200519@gmail.com>
2012-10-17 16:29:08 -0300

    add more colors

--- build.sh
+++ build.sh
@@ -46,13 +46,21 @@ then
 fi
 
 # colorization fix in Jenkins
-export CL_RED="\"\033[31m\""
-export CL_GRN="\"\033[32m\""
-export CL_YLW="\"\033[33m\""
-export CL_BLU="\"\033[34m\""
-export CL_MAG="\"\033[35m\""
-export CL_CYN="\"\033[36m\""
-export CL_RST="\"\033[0m\""
+export CL_RED="\"\033[31m""
+export CL_GRN="\"\033[32m""
+export CL_YLW="\"\033[33m""
+export CL_BLU="\"\033[34m""
+export CL_MAG="\"\033[35m""
+export CL_CYN="\"\033[36m""
+export CL_LRD="\"\033[1;31m""
+export CL_LGN="\"\033[1;32m""
+export CL_LYW="\"\033[1;33m""
+export CL_LBL="\"\033[1;34m""
+export CL_LMG="\"\033[1;35m""
+export CL_LCY="\"\033[1;36m""
+export CL_GRY="\"\033[1;30m""
+export CL_RST="\"\033[0m""
+
 
 cd $WORKSPACE
 rm -rf archive
